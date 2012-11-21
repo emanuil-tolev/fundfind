8f553bfb338665159191c38dbada9e7e7719c1aa
Emanuil Evgeniev Tolev <emanuil.tolev@gmail.com>
2012-11-21 22:03:21 +0000

    convert more of the base of IDFind into the base for FundFind

--- data-loader.py
+++ data-loader.py
@@ -3,9 +3,10 @@ import httplib, json
 f = open('data.json')
 j = json.loads(f.read())
 
-for r in j:
-    print json.dumps(r)
-    conn = httplib.HTTPConnection('localhost', '9200')
-    fullpath = "/fundfind/test"
-    conn.request('POST', fullpath, json.dumps(r))
-    print conn.getresponse().status
+def index(what):
+    for r in j:
+        print json.dumps(r)
+        conn = httplib.HTTPConnection('localhost', '9200')
+        fullpath = "/fundfind/" + what
+        conn.request('POST', fullpath, json.dumps(r))
+        print conn.getresponse().status
--- fundfind/static/js/fundfind.js
+++ fundfind/static/js/fundfind.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function() {
 	// clear the title from the front page search box
 	// or set title if box is empty
     var setTitle = function(event) {
-		var title = "/^(FundFind).*?$/";
+		var title = "Let's fund you...";
 		if (jQuery('.frontin').val() == "") {
 			jQuery('.frontin').val(title);
 		} else if (jQuery(this).val() == title) {
