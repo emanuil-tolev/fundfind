29cb223ddd2ee95d8a3b05a54aa7baa6302b91d1
Emanuil Tolev <emanuil@cottagelabs.com>
2013-02-21 15:38:56 +0000

    turn debugging off, this is running live on CL test server and I do not use that debugging UI right now anyway

--- fundfind/web.py
+++ fundfind/web.py
@@ -181,5 +181,5 @@ def outputJSON(results, record=False):
 
 if __name__ == "__main__":
     fundfind.dao.init_db()
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001)
 
