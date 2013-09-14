567f82c6baa2c014b262f818921f4a1328fa76b5
Emanuil Tolev <emanuil.tolev@gmail.com>
2013-09-14 11:55:35 +0100

    upgrade usage of linkifier; upgrade regex in jquery linkify plugin

--- fundfind/static/vendor/linkify/1.0-modified/jquery.linkify-1.0.js
+++ fundfind/static/vendor/linkify/1.0-modified/jquery.linkify-1.0.js
@@ -69,15 +69,14 @@
 
 (function($){
 
-  var noProtocolUrl = /(^|["'(\s]|&lt;)(www\..+?\..+?)((?:[:?]|\.+)?(?:\s|$)|&gt;|[)"',])/g,
-      httpOrMailtoUrl = /(^|["'(\s]|&lt;)((?:(?:https?|ftp):\/\/|mailto:).+?)((?:[:?]|\.+)?(?:\s|$)|&gt;|[)"',])/g,
-      linkifier = function ( html ) {
-          return html
+    var noProtocolUrl = /(^|["'(\s]|&lt;)(www\..+?\..+?)((?:[:?]|\.+)?(?:\s|$)|&gt;|[)"',])/g,
+    httpOrMailtoUrl = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/gi,
+        linkifier = function ( html ) {
+            return html
                       .replace( noProtocolUrl, '$1<a href="<``>://$2">$2</a>$3' )  // NOTE: we escape `"http` as `"<``>` to make sure `httpOrMailtoUrl` below doesn't find it as a false-positive
-                      .replace( httpOrMailtoUrl, '$1<a href="$2">$2</a>$3' )
+                      .replace(httpOrMailtoUrl, '<a href="$1">$1</a>')
                       .replace( /"<``>/g, '"http' );  // reinsert `"http`
         },
-
 
       linkify = $.fn.linkify = function ( cfg ) {
           if ( !$.isPlainObject( cfg ) )
