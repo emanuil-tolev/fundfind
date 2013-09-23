35321231a4a758333fbe84ca4f73daa9d1ea49b7
Emanuil Tolev <emanuil.tolev@gmail.com>
2013-09-23 20:37:41 +0100

    modify linkify plugin, generates broken unclickable links when text does not include protocol

--- fundfind/static/vendor/linkify/1.0-modified/jquery.linkify-1.0.js
+++ fundfind/static/vendor/linkify/1.0-modified/jquery.linkify-1.0.js
@@ -70,12 +70,16 @@
 (function($){
 
     var noProtocolUrl = /(^|["'(\s]|&lt;)(www\..+?\..+?)((?:[:?]|\.+)?(?:\s|$)|&gt;|[)"',])/g,
-    httpOrMailtoUrl = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/gi,
+    httpOrMailtoUrl = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/gi, // ET: new regex from stackoverflow, to extend links past right parenthesis ) symbol
         linkifier = function ( html ) {
             return html
-                      .replace( noProtocolUrl, '$1<a href="<``>://$2">$2</a>$3' )  // NOTE: we escape `"http` as `"<``>` to make sure `httpOrMailtoUrl` below doesn't find it as a false-positive
-                      .replace(httpOrMailtoUrl, '<a href="$1">$1</a>')
-                      .replace( /"<``>/g, '"http' );  // reinsert `"http`
+                      .replace(httpOrMailtoUrl, '<a href="http://$1">$1</a>') // ET: added http:// protocol - may break all other protocols
+
+                      // ET: commented these 2 lines out, were causing
+                      // problems with no-protocol links (completely
+                      // borked links generated, unclickable)
+                      //.replace( noProtocolUrl, '$1<a href="<``>://$2">$2</a>$3' )  // NOTE: we escape `"http` as `"<``>` to make sure `httpOrMailtoUrl` below doesn't find it as a false-positive
+                      //.replace( /"<``>/g, '"http' );  // reinsert `"http`
         },
 
       linkify = $.fn.linkify = function ( cfg ) {
